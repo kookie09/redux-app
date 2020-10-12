484325d1dd4d29ee6b950648cb1779d336b209f5
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-12 23:25:33 +1100

    restructuring actions

---
+++ src/store/actions/results.js
@@ -0,0 +1,26 @@
+import * as actionTypes from './actionsTypes';
+
+
+export const saveResult = (res) => {
+    return  {
+         type: actionTypes.STORE,
+         counter: res
+     };
+ }
+ 
+ export const storeResult = (res) => {
+ 
+     return (dispatch) => {
+         setTimeout(() => {
+             dispatch(saveResult(res));
+         }, 2000);
+     }
+ };
+ 
+ export const deleteResult = (index) => {
+     return {
+         type: actionTypes.DELETE,
+         value: index
+     };
+ };
+ 
